c0fb6bc5adb1b4035073b926cf38fc358ae49645
huy <dinhhuyams@gmail.com>
2020-01-02 16:05:13 +0200

    feat: add mock module

---
+++ jest-mock/src/__tests__/mock-fn.js
@@ -0,0 +1,17 @@
+import {thumbWar} from '../index'
+import {getWinner} from '../utils'
+
+// Mock module
+jest.mock('../utils', () => {
+	return {
+		getWinner: jest.fn((p1, p2) => p1),
+	}
+})
+
+test('return winner', () => {
+	const winner = thumbWar('Huy', 'Nhung')
+	expect(winner).toBe('Huy')
+
+	// cleanup
+	getWinner.mockReset()
+})
