87620210c29c8dfbf0375352ccee75de0ab0e64a
huy <dinhhuyams@gmail.com>
2020-01-03 13:15:27 +0200

    test: wrap CalculatorDisplay within ThemeProvider

--- jest/src/shared/__tests__/calculator-display.js
+++ jest/src/shared/__tests__/calculator-display.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import {render} from '@testing-library/react'
+import {ThemeProvider} from 'emotion-theming'
+import {dark} from '../../themes'
 import CalculatorDisplay from '../calculator-display'
 
 test('renders', () => {
-  const {container} = render(<CalculatorDisplay value="0" />)
+  const {container} = render(
+    <ThemeProvider theme={dark}>
+      <CalculatorDisplay value="0" />
+    </ThemeProvider>,
+  )
   expect(container).toMatchInlineSnapshot(`
     .emotion-0 {
       position: relative;
-      color: white;
-      background: #1c191c;
       line-height: 130px;
       font-size: 6em;
       -webkit-flex: 1;
       -ms-flex: 1;
       flex: 1;
+      color: white;
+      background: #1c191c;
     }
 
     <div>
       <div
-        class="emotion-0"
+        class="emotion-0 emotion-1"
       >
         <div
           class="autoScalingText"
