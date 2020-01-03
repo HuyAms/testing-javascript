4ad71db6a70cb70947d40880b5d48a3596bfdf5c
huy <dinhhuyams@gmail.com>
2020-01-03 13:08:31 +0200

    feat: add theming to calculator

--- jest/src/shared/calculator-display.js
+++ jest/src/shared/calculator-display.js
@@ -1,28 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
 import AutoScalingText from './auto-scaling-text'
 import {getFormattedValue} from './utils'
 
+const DisplayContainer = styled.div(
+    {
+      position: 'relative',
+      lineHeight: '130px',
+      fontSize: '6em',
+      flex: '1',
+    },
+    ({theme}) => ({
+      color: theme.displayTextColor,
+      background: theme.displayBackgroundColor,
+    }),
+)
+
 function CalculatorDisplay({value, ...props}) {
   const formattedValue = getFormattedValue(
-    value,
-    typeof window === 'undefined' ? 'en-US' : window.navigator.language,
+      value,
+      typeof window === 'undefined' ? 'en-US' : window.navigator.language,
   )
 
   return (
-    <div
-      {...props}
-      css={{
-        position: 'relative',
-        color: 'white',
-        background: '#1c191c',
-        lineHeight: '130px',
-        fontSize: '6em',
-        flex: '1',
-      }}
-    >
-      <AutoScalingText>{formattedValue}</AutoScalingText>
-    </div>
+      <DisplayContainer {...props}>
+        <AutoScalingText>{formattedValue}</AutoScalingText>
+      </DisplayContainer>
   )
 }
 
