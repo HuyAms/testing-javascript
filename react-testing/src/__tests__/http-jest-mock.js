597c6ef406a7c433208afbd9f6ebb927048b2498
huy <dinhhuyams@gmail.com>
2020-01-06 12:53:04 +0200

    test: mock HTTP requests with jest.mock in React Component Tests

--- react-testing/src/__tests__/http-jest-mock.js
+++ react-testing/src/__tests__/http-jest-mock.js
@@ -1,16 +1,26 @@
 import React from 'react'
-import {render, fireEvent} from '@testing-library/react'
-import {loadGreeting} from "../api";
-import {GreetingLoader} from "../greeting-loader-01-mocking";
+import {render, fireEvent, wait} from '@testing-library/react'
+import {loadGreeting as mockLoadGreeting} from '../api'
+import {GreetingLoader} from '../greeting-loader-01-mocking'
 
-jest.mock('../api')
+jest.mock('../api') // mock api module
 
-test('loads greetings on click', () => {
-  const {getByLabelText, getByText} = render(<GreetingLoader/>)
+test('loads greetings on click', async () => {
+  const testGreeting = 'TEST_GREETING'
+  mockLoadGreeting.mockResolvedValueOnce({data: {greeting: testGreeting}})
+
+  const {getByLabelText, getByText} = render(<GreetingLoader />)
   const nameInput = getByLabelText(/name/i)
   const loadButton = getByText(/load/i)
 
   nameInput.value = 'Marry'
 
   fireEvent.click(loadButton)
+
+  expect(mockLoadGreeting).toHaveBeenCalledWith('Marry')
+  expect(mockLoadGreeting).toHaveBeenCalledTimes(1)
+
+  await wait(() =>
+    expect(getByLabelText(/greeting/i)).toHaveTextContent(testGreeting),
+  )
 })
