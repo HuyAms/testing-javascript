4e36073879e17decb15216d1c266b27205a63013
huy <dinhhuyams@gmail.com>
2020-01-10 15:53:04 +0200

    test: a simple pure function

--- nodejs-testing/src/utils/__tests__/auth.exercise.js
+++ nodejs-testing/src/utils/__tests__/auth.exercise.js
@@ -1,10 +1,7 @@
 // Testing Pure Functions
 
-// 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
-
 // 🐨 import the function that we're testing
-// 💰 import {isPasswordAllowed} from '../auth'
+import {isPasswordAllowed} from '../auth'
 
 // 🐨 write tests for valid and invalid passwords
 // 💰 here are some you can use:
@@ -19,3 +16,16 @@ test.todo('remove me')
 // - abc123! // no uppercase letters
 // - ABC123! // no lowercase letters
 // - ABCdef123 // no non-alphanumeric characters
+
+test('isPAsswordAllowed returns true for valid passwords', () => {
+  expect(isPasswordAllowed('!aBc123')).toBe(true)
+})
+
+test('isPasswordAllowed returns false for invalid passwords', () => {
+  expect(isPasswordAllowed('a2c!')).toBe(false)
+  expect(isPasswordAllowed('123456!')).toBe(false)
+  expect(isPasswordAllowed('ABCdef!')).toBe(false)
+  expect(isPasswordAllowed('abc123!')).toBe(false)
+  expect(isPasswordAllowed('ABC123!')).toBe(false)
+  expect(isPasswordAllowed('ABCdef123')).toBe(false)
+})
