6daba91898921a392d1b70dccd41a5731db4fe0e
huy <dinhhuyams@gmail.com>
2020-01-10 16:09:50 +0200

    refactor: use jest-in-case to reduce duplication and improve test titles

--- nodejs-testing/src/utils/__tests__/auth.exercise.js
+++ nodejs-testing/src/utils/__tests__/auth.exercise.js
@@ -2,6 +2,7 @@
 
 // 🐨 import the function that we're testing
 import {isPasswordAllowed} from '../auth'
+import cases from 'jest-in-case'
 
 // 🐨 write tests for valid and invalid passwords
 // 💰 here are some you can use:
@@ -17,26 +18,65 @@ import {isPasswordAllowed} from '../auth'
 // - ABC123! // no lowercase letters
 // - ABCdef123 // no non-alphanumeric characters
 
-describe('isPAsswordAllowed only allows some passwords', () => {
-  const allowedPasswords = ['!aBc123']
-  const disallowedPasswords = [
-      'a2c!',
-      '123456!',
-      'ABCdef!',
-      'abc123!',
-      'ABC123!',
-      'ABCdef123',
-  ]
+cases(
+    'isPasswordAllowed: valid passwords',
+    (options) => {
+      expect(isPasswordAllowed(options.password)).toBe(true)
+    },
+    {
+      'valid password': {
+        password: '!aBc123'
+      }
+    }
+)
 
-  allowedPasswords.forEach(password => {
-    test(`allows ${password}`, () => {
-      expect(isPasswordAllowed(password)).toBe(true)
-    })
-  })
+cases(
+    'isPasswordAllowed: invalid passwords',
+    (options) => {
+      expect(isPasswordAllowed(options.password)).toBe(false)
+    },
+    {
+      'too short': {
+        password: 'a2c!'
+      },
+      'no letters': {
+        password: '123456!'
+      },
+      'no numbers': {
+        password: 'ABCdef!'
+      },
+      'no uppercase letters': {
+        password: 'abc123!'
+      },
+      'no lowercase letters': {
+        password: 'ABC123!'
+      },
+      'no non-alphanumeric characters': {
+        password: 'ABCdef123'
+      }
+    }
+)
 
-  disallowedPasswords.forEach(password => {
-    test(`disallows ${password}`, () => {
-      expect(isPasswordAllowed(password)).toBe(false)
-    })
-  })
-})
+// describe('isPAsswordAllowed only allows some passwords', () => {
+//   const allowedPasswords = ['!aBc123']
+//   const disallowedPasswords = [
+//       'a2c!',
+//       '123456!',
+//       'ABCdef!',
+//       'abc123!',
+//       'ABC123!',
+//       'ABCdef123',
+//   ]
+//
+//   allowedPasswords.forEach(password => {
+//     test(`allows ${password}`, () => {
+//       expect(isPasswordAllowed(password)).toBe(true)
+//     })
+//   })
+//
+//   disallowedPasswords.forEach(password => {
+//     test(`disallows ${password}`, () => {
+//       expect(isPasswordAllowed(password)).toBe(false)
+//     })
+//   })
+// })
